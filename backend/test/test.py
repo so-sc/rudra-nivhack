b3c11bda28f06926070db4ebbe897175ded45e2d
heimanpictures <heimanpicturesofficial@gmail.com>
2023-08-26 17:10:14 +0530

    Fixes

--- backend/test/test.py
+++ backend/test/test.py
@@ -54,3 +54,5 @@ async def websocket_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     uvicorn.run("test:app", host="0.0.0.0", port=8000, reload=True)
+
+
--- backend/test/test2.py
+++ backend/test/test2.py
@@ -4,33 +4,30 @@ import random
 from datetime import datetime, timedelta
 
 # Define constants
-num_files = 20000
+num_files = 10000
 start_date = datetime(2022, 1, 1)
 categories = ["Men's Fashion", "Women's Fashion", "Kid's Fashion"]
 products = ["T-shirt", "Jeans", "Dress", "Saree", "Kurta", "Shoes", "Sweater", "Accessories"]
 
-# Create a directory to store the CSV files
 if not os.path.exists("./backend/test/dataset"):
     os.makedirs("backend/test/dataset")
 
 filename = f"backend/test/dataset/data.csv"
-# Generate CSV files
 for i in range(num_files):
     
     with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file)
         
         # Write header
-        writer.writerow(['Pincode', 'Date', 'Category', 'Product'])
+        writer.writerow(['Pincode', 'Date', 'Category', 'Product', 'No product'])
         i = 0
+        current_date = start_date
         while i< num_files:
-            # Generate random data for each day in the year
-            current_date = start_date
             pincode = random.randint(560001, 591346)
             category = random.choice(categories)
             product = random.choice(products)
-
-            writer.writerow([pincode, current_date.strftime('%Y-%m-%d'), category, product])
+            nproducts = random.randint(0, 1000) + (random.randint(0, 200) if current_date.month in [1, 11, 12] else 0)
+            writer.writerow([pincode, current_date.strftime('%Y-%m-%d'), category, product, nproducts])
             i+=1
             current_date += timedelta(days=random.randint(1, 365))
 
