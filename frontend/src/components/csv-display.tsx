c19bf1e07bc7330b006e3b51a8edaa82c4be0653
Deveesh Shetty <deveeshshetty@gmail.com>
2023-08-26 19:04:13 +0530

    Adds Table and Forms

--- frontend/src/components/csv-display.tsx
+++ frontend/src/components/csv-display.tsx
@@ -1,3 +1,83 @@
+import { Button } from "@/components/ui/button"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+const invoices = [
+  {
+    product: "Product 1",
+    city: "Bangalore",
+    quantity: "56",
+    date: "13th September 2023",
+  },
+  {
+    product: "Product 2",
+    city: "Bangalore",
+    quantity: "72",
+    date: "13th September 2023",
+  },
+  {
+    product: "Product 3",
+    city: "Bangalore",
+    quantity: "43",
+    date: "13th September 2023",
+  },
+  {
+    product: "Product 4",
+    city: "Bangalore",
+    quantity: "92",
+    date: "13th September 2023",
+  },
+  {
+    product: "Product 5",
+    city: "Bangalore",
+    quantity: "31",
+    date: "13th September 2023",
+  },
+  {
+    product: "Product 6",
+    city: "Bangalore",
+    quantity: "55",
+    date: "13th September 2023",
+  },
+  {
+    product: "Product 7",
+    city: "Bangalore",
+    quantity: "12",
+    date: "13th September 2023",
+  },
+]
+
 export default function CSVDisplay() {
-  return <div></div>
+  return (
+    <>
+      <Table>
+        <TableCaption>Required Inventory.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Product</TableHead>
+            <TableHead>Region</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead>Quantity</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoice) => (
+            <TableRow key={invoice.product}>
+              <TableCell className="font-medium">{invoice.product}</TableCell>
+              <TableCell>{invoice.city}</TableCell>
+              <TableCell>{invoice.date}</TableCell>
+              <TableCell>{invoice.quantity}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  )
 }
--- frontend/src/components/predict-form.tsx
+++ frontend/src/components/predict-form.tsx
@@ -1,13 +1,48 @@
-import { FormEvent } from "react"
+"use client"
+
+import { DatePicker } from "@/components/date-picker"
+import { Button } from "@/components/ui/button"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Data } from "@/lib/types"
+import { FormEvent, useState } from "react"
 
 export default function PredictForm() {
-  function handleFormSubmit(formData: FormData) {
+  const [data, setData] = useState<Data>()
+
+  function handleFormSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
     console.log(formData)
   }
   return (
-    <div>
-      <form action={handleFormSubmit}>
-        <div></div>
+    <div className="w-full">
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex flex-col items-center gap-4">
+          <div>
+            <Select name="city">
+              <SelectTrigger className="w-[280px] mx-auto">
+                <SelectValue placeholder="Select City" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="bangalore">Bangalore</SelectItem>
+                <SelectItem value="chennai">Chennai</SelectItem>
+                <SelectItem value="delhi">Delhi</SelectItem>
+                <SelectItem value="mumbai">Mumbai</SelectItem>
+                <SelectItem value="bhopal">Bhopal</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div>
+            <DatePicker />
+          </div>
+          <Button type="submit">Predict Data</Button>
+        </div>
       </form>
     </div>
   )
