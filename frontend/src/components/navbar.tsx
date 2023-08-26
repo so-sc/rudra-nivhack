0b443d83776704d79e460d86c3dea04423c0390f
Deveesh Shetty <deveeshshetty@gmail.com>
2023-08-26 17:15:16 +0530

    Adds navbar

---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,26 @@
+import { ThemeToggle } from "@/components/ui/theme-toggle"
+import { siteConfig } from "@/config/siteConfig"
+import Link from "next/link"
+
+export function Navbar() {
+  return (
+    <nav className="flex fixed w-full backdrop-blur-sm items-center px-4 py-4 justify-between z-50">
+      <div className="flex gap-8 items-center">
+        <div>
+          <p className="scroll-m-20 text-xl font-semibold uppercase">
+            <Link href="/">{siteConfig.name}</Link>
+          </p>
+        </div>
+        <ul className="flex gap-4">
+          <li className="cursor-pointer hover:underline">
+            <Link href="/dashboard">Dashboard</Link>
+          </li>
+          <li className="cursor-pointer hover:underline">
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+      <ThemeToggle />
+    </nav>
+  )
+}
---
+++ frontend/src/config/siteConfig.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: "Rudra",
+  description: "Demand Forecasting and Inventory Mangement",
+}
